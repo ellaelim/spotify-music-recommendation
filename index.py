0dbef7aa315dad047f034e8ee7fb84cda040d48b
Ella Elimelech <ellaelimelech@Ellas-MacBook-Air-2.local>
2023-12-16 18:11:20 -0500

    Add comments to code

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+#import libraries
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import pandas as pd
@@ -7,13 +8,16 @@ import requests
 from bs4 import BeautifulSoup
 
 REDIRECT_URI = "http://localhost:8080"
+#Load Spotify API
 df = pd.read_csv('./keys.csv')
+#Retrieve client ID and client Secret
 CLIENT_ID, CLIENT_SECRET = tuple(df[df['KEY'] == 'CLIENT_ID']['VALUE'])[0], tuple(df[df['KEY'] == 'CLIENT_SECRET']['VALUE'])[0]
 
-
+#Retrieve top 5 songs
 api_settings = ('user-top-read', 5)
 scope, limit = api_settings
 
+#Spotify authentication
 def spotify_authenticate(client_id, client_secret, redirect_uri):
     return spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
                                                      client_secret=client_secret,
@@ -23,7 +27,7 @@ def spotify_authenticate(client_id, client_secret, redirect_uri):
 # Replace with your Spotify app credentials
 def print_track_details(track):
     print("Track Details:")
-    # Print nice readable columns for track details
+    # Print columns for track details
     print(f"Track ID: {track['id']}")
     print(f"Track Name: {track['name']}")
     print(f"Artist(s): {', '.join([artist['name'] for artist in track['artists']])}")
@@ -31,49 +35,53 @@ def print_track_details(track):
     print(f"Popularity: {track['popularity']}")
     print(f"Duration: {track['duration_ms']} ms")
 
+#Extract info from track
 def extract_info_from_track_title(title):
-    pattern = r'^(.*?) - (.*?)$'  # Pattern to extract 'song name - artist'
+#Pattern to extract 'song name - artist'
+    pattern = r'^(.*?) - (.*?)$'  
     match = re.search(pattern, title)
     if match:
         return match.groups()
     return title, "Unknown Artist"
 
+#Get user top
 def get_user_top_tracks(sp, limit=5):
     top_tracks = sp.current_user_top_tracks(limit=limit)['items']
     return [track['id'] for track in top_tracks]
 
-
+#Get song recommendations
 def get_song_recommendations(sp, seed_tracks, limit=5):
     recommendations = sp.recommendations(seed_tracks=seed_tracks, limit=limit)['tracks']
     return [(track['name'], track['artists'][0]['name']) for track in recommendations]
 
+#Main function
 def main():
     sp = spotify_authenticate(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
 
-    # Get the user's top tracks as seed tracks for recommendations
-    seed_tracks = get_user_top_tracks(sp, limit=limit)  # using the limit from the tuple
+    # Get the user's top tracks 
+    seed_tracks = get_user_top_tracks(sp, limit=limit) # Using the limit from the tuple
     if not seed_tracks:
         print("No top tracks available for seed data.")
         return
 
-    # Fetch and display recommendations
+    # Retrieve and display recommendations
     recommendations = get_song_recommendations(sp, seed_tracks)
     
     # Create a Pandas DataFrame of recommendations
     df = pd.DataFrame(recommendations, columns=['Song', 'Artist'])
     print(df)
 
-    # Example of using regex to extract info from the first track title (if available)
+    # Example of using a pattern to extract info from the first song title 
     if recommendations:
         song, artist = extract_info_from_track_title(recommendations[0][0])
         print(f"Extracted Info - Song: {song}, Artist: {artist}")
 
-    # Iterating through a sample track's details (if available)
+    # Show details of a sample song
     if seed_tracks:
         sample_track = sp.track(seed_tracks[0])
         print_track_details(sample_track)
 
-    # Create a bar chart of recommendation frequencies
+    # Create a bar chart of song recommendations
     df['Artist'].value_counts().plot(kind='barh')
     plt.title('Artist Frequency in Recommendations')
     plt.xlabel('Frequency')
